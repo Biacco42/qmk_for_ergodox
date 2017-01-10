caaee72f84701da6f01624d7e906003040701878
Biacco42 <secondimpact_2000@yahoo.co.jp>
2017-01-10 10:45:44 +0900

    Update keymap.c

--- keyboards/ergodox/keymaps/Norman/keymap.c
+++ keyboards/ergodox/keymaps/Norman/keymap.c
@@ -5,8 +5,8 @@
 
 #define BASE 0 // default layer
 #define META 1 // meta keys
-#define GAME 2 // game layer
-#define QWER 3 // QWERTY layer
+#define QWER 2 // QWERTY layer
+#define GAME 3 // game layer
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
@@ -93,8 +93,52 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_TRNS
 ),
+        
+/* Keymap 2: QWERTY layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |   1    |   2  |   3  |   4  |   5  |   6  |=>GAME|           |=>QWER|   7  |   8  |   9  |   0  |   -  |   ^    |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |  Tab   |   Q  |   W  |   E  |   R  |   T  |   [  |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   @    |
+ * |--------+------+------+------+------+------|   {  |           |  }   |------+------+------+------+------+--------|
+ * |  Alt   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   :    |
+ * |--------+------+------+------+------+------|   (  |           |  )   |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | \ / S  |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   | LCTL | LGUI |  App | Muhen| Henk |                                       | Left | Down |  Up  |Right |  \   |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,---------------.
+ *                                        |~META |~META |       |~META | ~META  |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      |      | Esc  |       | Del  | Back   |      |
+ *                                 |Space/| Tab/ |------|       |------| Space  |Enter/|
+ *                                 | Ctrl | Shift| Esc  |       | Del  |        |~META |
+ *                                 `--------------------'       `----------------------'
+ */
+// If it accepts an argument (i.e, is a function), it doesn't need KC_.
+// Otherwise, it needs KC_*
+[QWER] = KEYMAP(  // layer 0 : default
+        // left hand
+        KC_1,           KC_2,         KC_3,   KC_4,   KC_5,   KC_6,   KC_TRNS,
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_RBRC,
+        KC_LALT,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   S(KC_8),
+        KC_LCTL,        KC_LGUI,      KC_APP ,KC_MHEN,KC_HENK,
+                                              MO(META),       MO(META),
+                                                              KC_ESC,
+                                  CTL_T(KC_SPC),SFT_T(KC_TAB),KC_ESC,
+        // right hand
+             KC_TRNS,    KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,          KC_EQL,
+             KC_BSLS,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
+                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
+             S(KC_9),     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          SFT_T(KC_RO),
+                                  KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,          KC_JYEN,
+             MO(META),            MO(META),
+             KC_DELT,
+             KC_DELT,     KC_BSPC,LT(META, KC_ENT)
+),
 
-/* Keymap 2: game layer
+/* Keymap 3: game layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   1    |   2  |   3  |   4  |   5  |   6  |=>GAME|           |=>QWER|   7  |   8  |   9  |   0  |   -  |   ^    |
@@ -138,49 +182,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_DELT,     KC_BSPC,KC_ENT
 ),
 
-/* Keymap 3: QWERTY layer
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   1    |   2  |   3  |   4  |   5  |   6  |=>GAME|           |=>QWER|   7  |   8  |   9  |   0  |   -  |   ^    |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |  Tab   |   Q  |   W  |   E  |   R  |   T  |   [  |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   @    |
- * |--------+------+------+------+------+------|   {  |           |  }   |------+------+------+------+------+--------|
- * |  Alt   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   :    |
- * |--------+------+------+------+------+------|   (  |           |  )   |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | \ / S  |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LCTL | LGUI |  App | Muhen| Henk |                                       | Left | Down |  Up  |Right |  \   |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,---------------.
- *                                        |~META |~META |       |~META | ~META  |
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Esc  |       | Del  | Back   |      |
- *                                 |Space/| Tab/ |------|       |------| Space  |Enter/|
- *                                 | Ctrl | Shift| Esc  |       | Del  |        |~META |
- *                                 `--------------------'       `----------------------'
- */
-// If it accepts an argument (i.e, is a function), it doesn't need KC_.
-// Otherwise, it needs KC_*
-[QWER] = KEYMAP(  // layer 0 : default
-        // left hand
-        KC_1,           KC_2,         KC_3,   KC_4,   KC_5,   KC_6,   KC_TRNS,
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_RBRC,
-        KC_LALT,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   S(KC_8),
-        KC_LCTL,        KC_LGUI,      KC_APP ,KC_MHEN,KC_HENK,
-                                              MO(META),       MO(META),
-                                                              KC_ESC,
-                                  CTL_T(KC_SPC),SFT_T(KC_TAB),KC_ESC,
-        // right hand
-             KC_TRNS,    KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,          KC_EQL,
-             KC_BSLS,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
-                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
-             S(KC_9),     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          SFT_T(KC_RO),
-                                  KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,          KC_JYEN,
-             MO(META),            MO(META),
-             KC_DELT,
-             KC_DELT,     KC_BSPC,LT(META, KC_ENT)
-),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
