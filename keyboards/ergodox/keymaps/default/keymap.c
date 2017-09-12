9510c187e4bfc55af05082ca692fb4bdbebe23dd
Biacco42 <secondimpact_2000@yahoo.co.jp>
2017-09-12 13:44:58 +0900

    Update keymap.c

--- keyboards/ergodox/keymaps/default/keymap.c
+++ keyboards/ergodox/keymaps/default/keymap.c
@@ -148,9 +148,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |   1    |   2  |   3  |   4  |   5  |   6  |      |           |      |   7  |   8  |   9  |   0  |   -  |   ^    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  Alt   |      |      | Muhen| Henk |      |------|           |------| Left | Down |  Up  | Right|      |        |
+ * |  Alt   |  F1  |      | Muhen| Henk |      |------|           |------| Left | Down |  Up  | Right|      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LShift |  F2  |  F3  |  F4  |  F5  |  F6  |      |           |      |  F7  |  F8  |  F9  | F10  | F11  |  F1    |
+ * | LShift |  F2  |  F3  |  F4  |  F5  |  F6  |      |           |      |  F7  |  F8  |  F9  | F10  | F11  |  \ / S |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -166,7 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [META] = KEYMAP(
        KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
        KC_1,      KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_TRNS,
-       KC_TRNS,   KC_NO,   KC_NO,   KC_MHEN, KC_HENK, KC_NO,
+       KC_TRNS,   KC_F1,   KC_NO,   KC_MHEN, KC_HENK, KC_NO,
        KC_TRNS,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS,
        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                            KC_TRNS, RESET,
@@ -176,7 +176,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,  KC_NO,   KC_NO,   KC_PSCR, KC_NO,   KC_NO,   KC_PWR,
        KC_TRNS,  KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
                  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,
-       KC_TRNS,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F1,
+       KC_TRNS,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_TRNS,
                           KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_TRNS,
        KC_TRNS,  KC_TRNS,
        KC_TRNS,
